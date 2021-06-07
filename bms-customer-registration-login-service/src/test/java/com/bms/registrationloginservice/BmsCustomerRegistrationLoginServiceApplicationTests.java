c5f95d05bea9366185f540a8948a703bf16cb2c3
Selvamani <selvamani283@gmail.com>
2021-06-08 00:43:24 +0530

    BMS case study

---
+++ bms-customer-registration-login-service/src/test/java/com/bms/registrationloginservice/BmsCustomerRegistrationLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.registrationloginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsCustomerRegistrationLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
